c64773923f29f7b0f58c8bec6c65f3f911049eb2
murat kara <mail.muratkara@gmail.com>
2022-02-01 02:44:53 +0300

    working on JWT

--- blog-react/api/routes/auth.js
+++ blog-react/api/routes/auth.js
@@ -1,41 +1,85 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
-
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 //REGISTER
-router.post("/register", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(req.body.password, salt);
+router.post('/register', async (req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(req.body.password, salt);
 
-    const newUser = new User({
-      username: req.body.username,
-      email: req.body.email,
-      password: hash,
-    });
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hash,
+        });
 
-    const user = await newUser.save();
-    res.status(200).json(user);
-  } catch (err) {
-    res.status(409).json(err);
-  }
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(409).json(err);
+    }
 });
 
+let refreshTokens = [];
 //LOGIN
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong Username");
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            username: req.body.username,
+        });
+        const isPassTrue = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+        if (isPassTrue) {
+            const { password, ...others } = user._doc;
+            const accessToken = jwt.sign(others, process.env.JWT_TOKEN, {
+                expiresIn: '20s',
+            });
+            const refreshToken = jwt.sign(
+                others,
+                process.env.JWT_REFRESH_TOKEN
+            );
 
-    const isPassTrue = await bcrypt.compare(req.body.password, user.password);
-    !isPassTrue && res.status(400).json("Wrong Password");
+            refreshTokens.push(refreshToken);
+            res.json({
+                id: user._id,
+                username: user.username,
+                accessToken,
+                refreshToken,
+            });
+        } else {
+            res.status(400).json('WRONG PASSWORD.');
+        }
+    } catch (error) {
+        console.log(error);
+        res.status(505).json('err');
+    }
+});
 
-    const { password, ...others } = user._doc;
+//JWT REFRRESH TOKEN
+router.post('/refresh', (req, res) => {
+    const refreshToken = req.body.token;
+    if (!refreshToken) return res.status(401).json('You did not authenticate.');
+    if (!refreshTokens.includes(refreshToken))
+        return res.status(402).json('Refresh token`s not valid.');
 
-    res.status(200).json(others);
-  } catch (error) {
-    res.status(505).json(error);
-  }
+    jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN, (err, user) => {
+        if (err) return res.status(401).json('There is some error.');
+
+        refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+        const { password, ...others } = user;
+        const newAccessToken = jwt.sign(others, process.env.JWT_TOKEN, {
+            expiresIn: '20s',
+        });
+        const newRefreshToken = jwt.sign(others, process.env.JWT_REFRESH_TOKEN);
+        refreshTokens.push(newRefreshToken);
+        res.status(200).json({
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken,
+        });
+    });
 });
 
 module.exports = router;
--- blog-react/api/routes/users.js
+++ blog-react/api/routes/users.js
@@ -1,55 +1,94 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 //UPDATE
-router.put("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
-    }
-    try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.id,
-        {
-          $set: req.body,
-        },
-        { new: true }
-      );
+router.put('/:id', async (req, res) => {
+    if (req.body.userId === req.params.id) {
+        if (req.body.password) {
+            const salt = await bcrypt.genSalt(10);
+            req.body.password = await bcrypt.hash(req.body.password, salt);
+        }
+        try {
+            const updatedUser = await User.findByIdAndUpdate(
+                req.params.id,
+                {
+                    $set: req.body,
+                },
+                { new: true }
+            );
 
-      res.status(200).json(updatedUser);
-    } catch (error) {
-      res.status(500).json(error);
+            res.status(200).json(updatedUser);
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    } else {
+        res.status(401).json('Invalid UserID');
     }
-  } else {
-    res.status(401).json("Invalid UserID");
-  }
 });
 
 //DELETE
-router.delete("/:id", async (req, res) => {
-  if (req.body.userId === req.params.id) {
-    try {
-      await User.findOneAndDelete(req.params.id);
-      res.status(200).json("User's been deleted.");
-    } catch (error) {
-      res.status(500).json(error);
+// router.delete("/:id", async (req, res) => {
+//   if (req.body.userId === req.params.id) {
+//     try {
+//       await User.findOneAndDelete(req.params.id);
+//       res.status(200).json("User's been deleted.");
+//     } catch (error) {
+//       res.status(500).json(error);
+//     }
+//   } else {
+//     res.status(401).json("Invalid UserID");
+//   }
+// });
+
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader) {
+        const token = authHeader;
+
+        jwt.verify(token, process.env.JWT_TOKEN, (err, user) => {
+            if (err) {
+                res.status(401).json('TOKEN IS INVALID');
+            } else {
+                req.user = user;
+                next();
+            }
+        });
+    } else {
+        res.status(402).json('You are not authenticated.');
     }
-  } else {
-    res.status(401).json("Invalid UserID");
-  }
+};
+
+router.delete('/:id', verify, async (req, res) => {
+    try {
+        console.log(req.user._id);
+
+        if (req.user._id === req.params.id) {
+            try {
+                //await User.findOneAndDelete({_id: req.params.id});
+                res.status(200).json("User's been deleted.");
+            } catch (err) {
+                res.status(500).json(err);
+            }
+        } else {
+            res.status(405).json('INVALID REQUEST');
+        }
+    } catch (error) {
+        res.status(403).json(error);
+    }
 });
 
 //GET
-router.get("/:id", async (req, res) => {
-  try {
-    const user = await User.findById(req.params.id);
-    const { password, ...others } = user._doc;
-    res.status(200).json(others);
-  } catch (error) {
-    res.status(500).json(error);
-  }
+router.get('/:id', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const { password, ...others } = user._doc;
+        res.status(200).json(others);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
